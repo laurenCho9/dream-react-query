c1a4ccaf27362df7b00cc969b720f9ca0a3dc795
laurenCho9 <lauren.choux@gmail.com>
2024-03-10 19:19:20 +0900

    feat: 커스텀훅=> 리액트쿼리로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import MainProducts from "./components/MainProducts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <MainProducts />
+      <QueryClientProvider client={queryClient}>
+        <MainProducts />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
     </>
   );
 }
